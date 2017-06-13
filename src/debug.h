9f503b219127db4ec89b067c98b3228b0db993ba
pizmotality <aestreledge@gmail.com>
2017-06-13 01:35:16 -0400

    boot: bootable kernel skeleton
    basic library functions
    i8259 interrupt controller

---
+++ src/debug.h
@@ -0,0 +1,32 @@
+/* debug.h - Useful macros for debugging
+ */
+
+#ifndef _DEBUG_H
+#define _DEBUG_H
+
+#ifndef ASM
+
+#ifdef DEBUG
+#define ASSERT(EXP)                                                         \
+do {                                                                        \
+    if (!(EXP)) {                                                           \
+        printf(__FILE__ ":%u: Assertion `" #EXP "\' failed.\n", __LINE__);  \
+    }                                                                       \
+} while (0)
+
+#define debugf(...)                                                         \
+do {                                                                        \
+    printf(__FILE__ ":%u: ", __LINE__);                                     \
+    printf(__VA_ARGS__);                                                    \
+} while (0)
+
+#else
+#define ASSERT(EXP)                                                         \
+    while (0)
+#define debugf(...)                                                         \
+    while (0)
+#endif
+
+#endif
+
+#endif /* _DEBUG_H */
