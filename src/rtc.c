0aebe05edbe700b16d4458b0c022dcfce6905478
pizmotality <aestreledge@gmail.com>
2017-06-25 02:28:29 -0400

    rtc: support changing rtc frequency

--- src/rtc.c
+++ src/rtc.c
@@ -5,48 +5,89 @@
 #include "i8259.h"
 #include "lib.h"
 
+int32_t status;
+
 void init_rtc() {
     cli();
 
-    outb(RTC_REGISTER_B, RTC_PORT);
-    uint8_t prev = inb(RTC_DATA_PORT);
-    prev = (prev & 0xFB) | 0x43;
-    outb(RTC_REGISTER_B, RTC_PORT);
-    outb(prev, RTC_DATA_PORT);
+    uint8_t prev;
+    outb(RTC_CR_B, RTC_PORT);
+    prev = inb(RTC_DATA_PORT);
+    outb(RTC_CR_B, RTC_PORT);
+    outb(prev | 0x40, RTC_DATA_PORT);
+
+    outb(RTC_CR_A, RTC_PORT);
+    prev = inb(RTC_DATA_PORT);
+    outb(RTC_CR_A, RTC_PORT);
+    outb((prev & 0xF0) | 0x0F, RTC_DATA_PORT);
 
     sti();
+
+    status = RTCCLOSE;
 }
 
 void handle_rtc() {
     cli();
     send_eoi(IRQ_RTC);
 
-    outb(RTC_REGISTER_C, RTC_PORT);
+    outb(RTC_CR_C, RTC_PORT);
     inb(RTC_DATA_PORT);
+
+    status &= ~RTCSET;
 
     sti();
 }
 
 int32_t read_rtc(int32_t fd, int8_t* buf, int32_t nbytes) {
-    return -1;
+    status |= RTCSET;
+    while (status & RTCSET);
+
+    return 0;
 }
 
 int32_t write_rtc(const int8_t* buf, int32_t nbytes) {
-    return -1;
+    uint32_t freq = *(uint32_t*)buf;
+    return set_rtc_freq(freq);
 }
 
 int32_t open_rtc() {
-    return -1;
+    if (status)
+        return -1;
+
+    status = RTCOPEN;
+    return 0;
 }
 
 int32_t close_rtc() {
-    return -1;
+    if (!status)
+        return -1;
+
+    status = RTCCLOSE;
+    return 0;
 }
 
-void set_rtc_freq(uint32_t freq) {
+int32_t set_rtc_freq(uint32_t freq) {
     if (freq & (freq - 1))
-        return;
+        return -1;
 
     if (freq & 0xFFFFF800)
-        return;
+        return -1;
+
+    uint32_t divider = 0xF;
+    for (; divider > 0x5; --divider, freq = freq >> 1) {
+        if (freq & 0x2) {
+            cli();
+
+            outb(RTC_CR_A, RTC_PORT);
+            uint8_t prev = inb(RTC_DATA_PORT);
+            outb(RTC_CR_A, RTC_PORT);
+            outb((prev & 0xF0) | divider, RTC_DATA_PORT);
+
+            sti();
+
+            return 0;
+        }
+    }
+
+    return -1;
 }
--- src/rtc.h
+++ src/rtc.h
@@ -9,9 +9,9 @@
 #define RTC_PORT        0x70
 #define RTC_DATA_PORT   0x71
 
-#define RTC_REGISTER_A  0x0A
-#define RTC_REGISTER_B  0x0B
-#define RTC_REGISTER_C  0x0C
+#define RTC_CR_A    0x8A
+#define RTC_CR_B    0x8B
+#define RTC_CR_C    0x8C
 
 typedef struct time_t {
     uint8_t second;
@@ -23,6 +23,10 @@ typedef struct time_t {
     uint8_t century;
 } time_t;
 
+#define RTCCLOSE    0x0
+#define RTCOPEN     0x1
+#define RTCSET      0x2
+
 void init_rtc();
 
 void handle_rtc();
@@ -32,6 +36,6 @@ int32_t write_rtc(const int8_t* buf, int32_t nbytes);
 int32_t open_rtc();
 int32_t close_rtc();
 
-void set_rtc_freq(uint32_t freq);
+int32_t set_rtc_freq(uint32_t freq);
 
 #endif /* _RTC_H */
