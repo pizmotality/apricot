11af5eeb1a0abbc42bd3bdf8b4875378c09211c0
pizmotality <aestreledge@gmail.com>
2017-09-15 02:42:54 -0400

    qemu: perform i386 emulation on qemu-system-x86_64
    qemu-arch-extra package no longer required

--- src/debug.sh
+++ src/debug.sh
@@ -2,5 +2,5 @@
 
 GDB=$([ $(uname -s) = "Darwin" ] && echo "i386-elf-gdb" || echo "gdb")
 
-qemu-system-i386 -m 256 -drive format=raw,file=./img/os.img -no-shutdown -no-reboot -s -S &
+qemu-system-x86_64 -cpu athlon -m 256 -drive format=raw,file=./img/os.img -no-shutdown -no-reboot -s -S &
 $GDB ./img/bootimg -command gdb.init
