3bdfc0ee0633b8a24a7b242fd24bff24a32f748b
pizmotality <aestreledge@gmail.com>
2017-09-21 18:40:56 -0400

    process: change pid field to be signed
    negative pid used to indicate errors
    process struct should only contain valid pid values

--- src/process.h
+++ src/process.h
@@ -42,7 +42,7 @@ typedef struct pcb_t pcb_t;
 
 typedef struct pcb_t {
     uint32_t state;
-    uint32_t pid;
+    int32_t pid;
     uint32_t parent_esp;
     uint32_t parent_ebp;
     uint32_t parent_eip;
